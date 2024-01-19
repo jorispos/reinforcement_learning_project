29830a05ac52e281735c5c49ce3e3fd870e249be
Joris Postmus <jorispostmus@gmail.com>
2024-01-19 15:22:12 +0100

    o

--- lunar_lander_skeleton.py
+++ lunar_lander_skeleton.py
@@ -5,6 +5,7 @@ observation, info = env.reset(seed=42)
 
 for _ in range(1000):
    action = env.action_space.sample()  # this is where you would insert your policy
+
    observation, reward, terminated, truncated, info = env.step(action)
 
    if terminated or truncated:
